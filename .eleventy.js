27b6a88cf1677993e5e17544d57d433ecd6bdda4
Lonny Gomes <code@lonnygomes.com>
2020-11-09 23:19:14 -0500

    feat: add date filters to eleventy config
    adds filter for full date, and partial dates for month, year, and day
    
    refs #12

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,25 @@
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/images');
     
+    eleventyConfig.addFilter('fullDate', (dateStr) =>
+        DateTime.fromRFC2822(dateStr).toLocaleString(DateTime.DATE_HUGE)
+    );
+
+    eleventyConfig.addFilter('dateDay', (dateStr) =>
+        DateTime.fromRFC2822(dateStr).toFormat('dd')
+    );
+
+    eleventyConfig.addFilter('dateMonth', (dateStr) =>
+        DateTime.fromRFC2822(dateStr).toFormat('MMM')
+    );
+
+    eleventyConfig.addFilter('dateYear', (dateStr) =>
+        DateTime.fromRFC2822(dateStr).toFormat('yyyy')
+    );
+
     return {
         dir: {
             input: 'src',
